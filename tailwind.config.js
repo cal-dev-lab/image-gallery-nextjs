174f47757c1087a74aae9d1c1f400cc7c0a6241e
Callum Lees <hi@c-l.dev>
2022-07-03 12:57:27 +0100

    Added Poppins font

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    fontFamily: {
+      poppins: ['poppins', 'Poppins', 'sans-serif'],
+    },
   },
   plugins: [require('@tailwindcss/aspect-ratio')],
 }
