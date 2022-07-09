fd1882243b8ec3db7b43323c27ce692fa4d4fc87
Callum Lees <hi@c-l.dev>
2022-07-09 22:12:28 +0100

    Removed font

--- pages/index.tsx
+++ pages/index.tsx
@@ -39,7 +39,7 @@ export default function Gallery({ images }: { images: Image[] }) {
 
     <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
 
-      <h3 className='text-6xl mb-10 font-baunk underline'>
+      <h3 className='text-6xl mb-10'>
         Gallery
       </h3>
 
